4ed7b044756880fdd65e0a69bdffabed754559d7
prajodh <prajodhpragaths@gmail.com>
2024-04-06 21:20:42 -0400

    the send email after payment functionaility is done

--- controllers/renting.js
+++ controllers/renting.js
@@ -1,5 +1,6 @@
 const carListings = require("../models/carListings")
 const rentItems = require("../models/renting")
+const login = require("../models/login")
 const {StatusCodes} = require("http-status-codes")
 const stripe = require('stripe')(process.env.STRIPE_KEY)
 
@@ -19,12 +20,32 @@ const stripePayment = async(req, res) =>{
     // const calculateOrderAmount = () =>{
     //     // add code to do payment
     // }
-
+    const total = 3000
     const paymentIntent = await stripe.paymentIntents.create({
-        amount:3000,
+        amount:total,
         currency: 'usd'
     })
+          
     res.status(StatusCodes.OK).json({message:"success", data:{clientSecret:paymentIntent.client_secret}, status_code:StatusCodes.OK})
+
+
+}
+
+
+const sendPaymentEmail = async(req,res)=>{ 
+    const user = await login.findOne({_id:req.user.userID})   
+    const text = "your payment for the item for the cost of "+req.body.total+" is done"
+    console.log(user.email)
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+          to: user.email, 
+          from: 'hpass609@gmail.com', 
+          subject: 'Payment Done',
+          text: text,
+    }
+    const info = sgMail.send(msg)          
+    res.status(StatusCodes.OK).json({message:"success", data:"email has been sent", status_code:StatusCodes.OK})
 
 
 }
@@ -32,4 +53,4 @@ const stripePayment = async(req, res) =>{
 
 
 
-module.exports = {rentItem, stripePayment}
+module.exports = {rentItem, stripePayment,sendPaymentEmail}
--- routes/renting.js
+++ routes/renting.js
@@ -2,9 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 
-const {rentItem, stripePayment} = require("../controllers/renting")
+const {rentItem, stripePayment, sendPaymentEmail} = require("../controllers/renting")
 router.post("/rentItem", rentItem)
 router.post("/stripePayment", stripePayment)
+router.post("/sendPaymentEmail", sendPaymentEmail)
 
 
 module.exports = router
