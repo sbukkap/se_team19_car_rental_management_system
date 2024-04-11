cd5efa1bcedda6801f2cd8737827c0d7a64286b4
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-04-11 16:13:30 -0400

    cart

--- controllers/shoppingCart.js
+++ controllers/shoppingCart.js
@@ -9,8 +9,10 @@ const isShoppingCartPresent = async (req, res) => {
     const shoppingCartUser = await shoppingCart.find({ user_id: user, isDeleted: true });
     // console.log(shoppingCartUser.length)
     if (shoppingCartUser.length === 0) {
+        console.log('no cart')
         res.status(StatusCodes.OK).json({ message: "No shopping cart", data: { shoppingCart: false }, status_code: StatusCodes.OK });
     } else {
+        console.log('present')
         res.status(StatusCodes.OK).json({ message: "Shopping cart present", data: { shoppingCart: true, id: shoppingCartUser._id }, status_code: StatusCodes.OK });
     }
 }
@@ -18,7 +20,7 @@ const isShoppingCartPresent = async (req, res) => {
 
 
 const createShoppingcart = async(req, res)=>{
-    console.log(req.body.items)
+    console.log(req.body)
     const shoppingCart_doc = await shoppingCart.create(req.body)
     res.status(StatusCodes.CREATED).json({message:"success", data: shoppingCart_doc, status_code:StatusCodes.CREATED})
 }
@@ -69,23 +71,78 @@ const get_Shoppingcart = async(req, res)=>{
 //     }
 // };
 
-const updateShoppingcart = async(req, res)=>{
+// const updateShoppingcart = async(req, res)=>{
+//     try {
+//         const userId = req.params.userId;
+//         const shoppingCartUpdate = req.body;
+//         if (!shoppingCartUpdate.items) {
+//             throw new Error('No items provided for update');
+//         }
+//         // Update the shopping cart with the new items array
+//         // console.log(shoppingCartUpdate.items)
+//         const shoppingCartUpdated = await shoppingCart.findOneAndUpdate({ user_id: userId }, { items: shoppingCartUpdate.items }, { new: true, runValidators: true });
+//         res.status(StatusCodes.OK).json({ message: "Update successful", data: shoppingCartUpdated, status_code: StatusCodes.OK });
+//     } catch (error) {
+//         console.error('Error updating shopping cart:', error);
+//         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Error updating shopping cart", data: {}, status_code: StatusCodes.INTERNAL_SERVER_ERROR });
+//     }
+// };
+
+
+// const updateShoppingcart = async (req, res) => {
+//     try {
+//         const userId = req.params.userId;
+//         const shoppingCartUpdate = req.body;
+//         if (!shoppingCartUpdate.items) {
+//             throw new Error('No items provided for update');
+//         }
+
+//         // Create a new instance of the ShoppingCart model
+//         const newShoppingCart = new shoppingCart({
+//             user_id: userId,
+//             items: shoppingCartUpdate.items
+//             // Add other fields if necessary
+//         });
+
+//         // Save the new shopping cart document to the database
+//         const shoppingCartSaved = await newShoppingCart.save();
+
+//         // Return the newly saved document as response
+//         res.status(StatusCodes.OK).json({ message: "Insert successful", data: shoppingCartSaved, status_code: StatusCodes.OK });
+//     } catch (error) {
+//         console.error('Error inserting shopping cart:', error);
+//         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Error inserting shopping cart", data: {}, status_code: StatusCodes.INTERNAL_SERVER_ERROR });
+//     }
+// };
+
+const updateShoppingcart = async (req, res) => {
     try {
         const userId = req.params.userId;
         const shoppingCartUpdate = req.body;
         if (!shoppingCartUpdate.items) {
             throw new Error('No items provided for update');
         }
-        // Update the shopping cart with the new items array
-        // console.log(shoppingCartUpdate.items)
-        const shoppingCartUpdated = await shoppingCart.findOneAndUpdate({ user_id: userId }, { items: shoppingCartUpdate.items }, { new: true, runValidators: true });
-        res.status(StatusCodes.OK).json({ message: "Update successful", data: shoppingCartUpdated, status_code: StatusCodes.OK });
+
+        const ShoppingCart = await shoppingCart.findOneAndUpdate(
+            { user_id: userId }, // Query: Find document with user_id
+            { $push: { items: { $each: shoppingCartUpdate.items } } }, // Update: Push each item to items array
+            { new: true, runValidators: true } // Options: Return updated document
+        );
+
+        // Check if shopping cart document exists
+        if (!ShoppingCart) {
+            // If shopping cart document doesn't exist, handle the case (e.g., return 404)
+            res.status(StatusCodes.NOT_FOUND).json({ message: "Shopping cart not found for user", data: {}, status_code: StatusCodes.NOT_FOUND });
+            return;
+        }
+
+        // Return the updated document as response
+        res.status(StatusCodes.OK).json({ message: "Update successful", data: ShoppingCart, status_code: StatusCodes.OK });
     } catch (error) {
         console.error('Error updating shopping cart:', error);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Error updating shopping cart", data: {}, status_code: StatusCodes.INTERNAL_SERVER_ERROR });
     }
 };
-
 
 
 const deleteItemShoppingcart = async(req, res)=>{
@@ -96,5 +153,8 @@ const deleteItemShoppingcart = async(req, res)=>{
     res.status(StatusCodes.OK).json({message:"Shopping cart deleted", data: shoppingCartDelete, status_code:StatusCodes.OK})
 }
 
+const deleteUserShoppingCart = async(req, res)=>{
+    
+}
 
 module.exports = {isShoppingCartPresent , createShoppingcart, get_Shoppingcart, updateShoppingcart, deleteItemShoppingcart}
