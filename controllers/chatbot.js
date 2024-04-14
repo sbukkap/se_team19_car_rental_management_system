6ed49c2c21c1206db8affe2d9d5cc19a2af54403
prajodh <prajodhpragaths@gmail.com>
2024-04-14 08:58:00 -0400

    fixed the chatbot

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -1,18 +1,26 @@
-const {StatusCodes} = require("http-status-codes")
-const OpenAIApi = require("openai"); 
+const { StatusCodes } = require("http-status-codes");
+const OpenAI = require("openai");
+require("dotenv").config()
 
 
-const chatbot = async(req,res)=>{
-    const openai = new OpenAIApi({
-    apiKey: process.env.OPENAI_API_KEY,
-    });
-    const { message } = req.body
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages:[{"role":"user", "content":message}]
+
+const chatbot = async (req, res) => {
+    console.log(process.env.OPENAI_API_KEY)
+     const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
      });
-    res.status(StatusCodes.OK).json({message:"success",data:completion.choices[0].message, status_code:StatusCodes.OK})
 
-}
+    const message = [];
+    message.push(req.body.message)
+    const completion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: message[0]  }],
+    model: "gpt-3.5-turbo",
+  });
 
-module.exports = {chatbot}
+  console.log(completion.choices[0]);
+
+}    
+
+
+module.exports = { chatbot };
+
